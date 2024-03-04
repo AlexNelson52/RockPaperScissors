3af0ccf85e270970ae1dcef47e9884687bd3c33d
Alex N <77512968+AlexNelson52@users.noreply.github.com>
2024-03-04 09:32:22 -0500

    added a counter

--- main.js
+++ main.js
@@ -2,49 +2,59 @@
 
 const rockPaperScissor = ['rock', 'paper', 'scissors'];
 
-
 function computerChoice() {
   return rockPaperScissor[Math.floor(Math.random() * rockPaperScissor.length)];
 }
 
-
-
 const h1Text = document.getElementById("header")
+const playerScore = document.getElementById("playerScore")
+const computerScore = document.getElementById("computerScore")
+const drawScore = document.getElementById("drawScore")
 
-
-function rockInput() {
+function playerInput(playerChoice) {
   const computer = computerChoice();
-  if (rockPaperScissor[0] === computer) {
+  if (playerChoice === computer) {
+    draws();
     return (h1Text.innerText = `It's a tie you both picked ${computer}`)
-  } else if (rockPaperScissor[0] === 'rock' && computer === 'scissors') {
-    return (h1Text.innerText = `You picked Rock computer picked ${computer} you win!`)
-  } else if (rockPaperScissor[0] === 'rock' && computer === 'paper') {
-    return ((h1Text.innerText = `You picked Rock computer picked ${computer} you Lose!`))
   }
-}
-
-
-function paperInput() {
-  const computer = computerChoice();
-  if (rockPaperScissor[1] === computer) {
-    return (h1Text.innerText = `It's a tie you both picked ${computer}`)
-  } else if (rockPaperScissor[1] === 'paper' && computer === 'rock') {
-    return (h1Text.innerText = `You picked Paper computer picked ${computer} you win!`)
-  } else if (rockPaperScissor[1] === 'paper' && computer === 'scissors') {
-    return (h1Text.innerText = `You picked Paper computer picked ${computer} you Lose!`)
+  if (playerChoice === 'scissors' && computer === 'paper') {
+    score();
+    return (h1Text.innerText = `You picked Scissors computer picked ${computer} you WIN!`)
   }
-}
-
-function scissorsInput() {
-  const computer = computerChoice();
-  if (rockPaperScissor[2] === computer) {
-    return (h1Text.innerText = `It's a tie you both picked ${computer}`)
-  } else if (rockPaperScissor[2] === 'scissors' && computer === 'paper') {
-    return (h1Text.innerText = `You picked Scissors computer picked ${computer} you win!`)
-  } else if (rockPaperScissor[2] === 'scissors' && computer === 'rock') {
+  if (playerChoice === 'scissors' && computer === 'rock') {
+    score2();
     return (h1Text.innerText = `You picked Scissors computer picked ${computer} you Lose!`)
   }
+  if (playerChoice === 'rock' && computer === 'paper') {
+    score2();
+    return (h1Text.innerText = `You picked Rock computer picked ${computer} you Lose!`)
+  }
+  if (playerChoice === 'rock' && computer === 'scissors') {
+    score();
+    return (h1Text.innerText = `You picked Rock computer picked ${computer} you WIN!`)
+  }
+  if (playerChoice === 'paper' && computer === 'rock') {
+    score();
+    return (h1Text.innerText = `You picked Rock computer picked ${computer} you WIN!`)
+  }
+  if (playerChoice === 'paper' && computer === 'scissors') {
+    score2();
+    return (h1Text.innerText = `You picked Paper computer picked ${computer} you Lose`)
+  }
 }
 
+function score() {
+  let playerScoreNumber = Number(playerScore.innerText)
+  playerScore.innerText = playerScoreNumber + 1
+}
 
-console.log(computerChoice())
+function score2() {
+  let computerScores = Number(computerScore.innerText)
+  computerScore.innerText = computerScores + 1
+
+}
+
+function draws() {
+  let drawScores = Number(drawScore.innerText)
+  drawScore.innerText = drawScores + 1
+}
